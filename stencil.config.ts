83204a58714270c9fd6b1c2cf490dc4830730d64
Tim <t.walz@freenet.de>
2020-05-05 22:14:16 +0200

    Updating base URL config

--- stencil.config.ts
+++ stencil.config.ts
@@ -21,7 +21,7 @@ export const config: Config = {
       type: 'www',
       // comment the following line to disable service workers in production
       serviceWorker: null,
-      baseUrl: 'https://covapp.charite.de',
+      baseUrl: 'https://covapp.drk-brandenburg.de',
     },
   ],
   testing: {
